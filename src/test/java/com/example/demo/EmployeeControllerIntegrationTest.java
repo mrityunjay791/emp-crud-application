794755013ce2f6894c62f8fbeb00e4d463a96d26
Mrityunjay <mrityunjay.kumar86@T-Mobile.com>
2020-12-21 15:17:43 +0530

    Minor change.

---
+++ src/test/java/com/example/demo/EmployeeControllerIntegrationTest.java
@@ -0,0 +1,91 @@
+package com.example.demo;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
+
+import com.example.demo.model.Employee;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CrudapplicationApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EmployeeControllerIntegrationTest {
+     @Autowired
+     private TestRestTemplate restTemplate;
+
+     @LocalServerPort
+     private int port;
+
+     private String getRootUrl() {
+         return "http://localhost:" + port + "/springboot-crud-rest/api/v1";
+     }
+
+     @Test
+     public void contextLoads() {
+
+     }
+
+     @Test
+     public void testGetAllEmployees() {
+     HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/employees",
+        HttpMethod.GET, entity, String.class);  
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void testGetEmployeeById() {
+        Employee employee = restTemplate.getForObject(getRootUrl() + "/employees/1", Employee.class);
+        System.out.println(employee.getFirstName());
+        assertNotNull(employee);
+    }
+
+    @Test
+    public void testCreateEmployee() {
+        Employee employee = new Employee();
+        employee.setEmailId("mrityunjay@gmail.com");
+        employee.setFirstName("mk");
+        employee.setLastName("kumar");
+        ResponseEntity<Employee> postResponse = restTemplate.postForEntity(getRootUrl() + "/employees", employee, Employee.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+    }
+
+    @Test
+    public void testUpdateEmployee() {
+        int id = 1;
+        Employee employee = restTemplate.getForObject(getRootUrl() + "/employees/" + id, Employee.class);
+        employee.setFirstName("admin1");
+        employee.setLastName("admin2");
+        restTemplate.put(getRootUrl() + "/employees/" + id, employee);
+        Employee updatedEmployee = restTemplate.getForObject(getRootUrl() + "/employees/" + id, Employee.class);
+        assertNotNull(updatedEmployee);
+    }
+
+    @Test
+    public void testDeleteEmployee() {
+         int id = 2;
+         Employee employee = restTemplate.getForObject(getRootUrl() + "/employees/" + id, Employee.class);
+         assertNotNull(employee);
+         restTemplate.delete(getRootUrl() + "/employees/" + id);
+         try {
+              employee = restTemplate.getForObject(getRootUrl() + "/employees/" + id, Employee.class);
+         } catch (final HttpClientErrorException e) {
+              assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+         }
+    }
+}
